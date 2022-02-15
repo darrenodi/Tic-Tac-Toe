2984a64c5d721952f032e6d68e9deb89cba019cf
darrenodi <tantheta85@gmail.com>
2022-02-16 00:05:32 +0100

    Gameboard creation

---
+++ script.js
@@ -0,0 +1,28 @@
+const gameBoard = (() => {
+  const gameBoard = [];
+ 
+  return {
+    gameBoard,
+  };
+})()
+
+const displayController = (() => {
+  const displayController = ["X","O"];
+  var count = 1;
+  while(count<10) {
+    const getDiv = document.getElementById("gameboard");
+    var randomItem = displayController[Math.floor(Math.random()*displayController.length)];
+    getDiv.innerHTML += "<div class=\"gameboarditem\"><button class=\"tikbutton\"></button></div>";
+
+    count++;
+  }
+
+  return {
+    displayController,
+  };
+})()
+
+const Player = (name) => {
+  return {name};
+}
+
